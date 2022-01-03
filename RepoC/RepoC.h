d42edb598641900c26c0919b23d0ff8ebcc9a89c
Mehul Srivastava <mehul.srivastava@zomato.com>
2022-01-03 17:28:31 +0530

    Initial

---
+++ RepoC/RepoC.h
@@ -0,0 +1,18 @@
+//
+//  RepoC.h
+//  RepoC
+//
+//  Created by Mehul Srivastava on 30/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RepoC.
+FOUNDATION_EXPORT double RepoCVersionNumber;
+
+//! Project version string for RepoC.
+FOUNDATION_EXPORT const unsigned char RepoCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RepoC/PublicHeader.h>
+
+
---
+++ RepoCSource/RepoC.swift
@@ -0,0 +1,18 @@
+//
+//  RepoC.swift
+//  RepoC
+//
+//  Created by Mehul Srivastava on 30/12/21.
+//
+
+import Foundation
+
+open class C {
+    
+    public init() {}
+    
+    public func callC() {
+        print("C")
+    }
+    
+}
